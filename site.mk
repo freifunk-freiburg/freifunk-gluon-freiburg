2e9a444a1d490fa6d0571e11cace5836dd21995d
FreifunkUFO <ufo@rund.freifunk.net>
2015-01-29 18:49:19 +0100

    uebernahme von aktuellen daten von freifunk altmrf

--- site.mk
+++ site.mk
@@ -4,17 +4,18 @@ GLUON_SITE_PACKAGES := \
 	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
+	gluon-authorized-keys \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-next-node \
+	gluon-neighbour-info \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
@@ -23,7 +24,11 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_CHECKOUT := v2014.4
+# Allow overriding the checkout from the command line
+GLUON_CHECKOUT ?= $(DEFAULT_GLUON_CHECKOUT)
+
+DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
